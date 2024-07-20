12fc69e59913616dffd0a04621fa69344666f67a
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-07-20 22:00:49 +0300

    feat: Initialize db and load environment variables in main.go

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import "github.com/ssanyoq/go-boilerplate/src/routers"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/ssanyoq/go-boilerplate/src/db"
+	"github.com/ssanyoq/go-boilerplate/src/routers"
+	"log"
+	"os"
+)
 
 func main() {
-	router := routers.NewRoutes()
-	router.Run("localhost:8000")
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
 
+	setModeFromEnv()
+	db.Init()
+
+	hostname := os.Getenv("HOST_NAME")
+	router := routers.NewRoutes()
+
+	log.Printf("Go to http://%s to use your stuff!", hostname)
+	router.Run(hostname)
+
+}
+
+func setModeFromEnv() {
+	switch os.Getenv("ENV") {
+	case "PRODUCTION":
+		gin.SetMode(gin.ReleaseMode)
+	}
 }
