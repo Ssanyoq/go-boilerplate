51f2fe88e673e87dca723ebcd6550170f7d8a323
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-08-03 10:49:16 +0300

    feat: Cleanup and fix DB hooks for user creation and updates

--- src/models/user.go
+++ src/models/user.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"github.com/go-gorp/gorp"
-	"log"
 	"time"
 )
 
@@ -15,14 +14,13 @@ type User struct {
 	CreatedAt int64  `db:"created_at" json:"createdAt"`
 }
 
-func (u User) PreInsert(s gorp.SqlExecutor) error {
+func (u *User) PreInsert(s gorp.SqlExecutor) error {
 	u.CreatedAt = time.Now().UnixNano()
 	u.UpdatedAt = u.CreatedAt
-	log.Print("ME WORKIII") // TODO
 	return nil
 }
 
-func (u User) PreUpdate(s gorp.SqlExecutor) error {
+func (u *User) PreUpdate(s gorp.SqlExecutor) error {
 	u.UpdatedAt = time.Now().UnixNano()
 	return nil
 }
