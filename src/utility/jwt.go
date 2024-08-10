f56708c30e1c818e106d6169ad6f7f5eda7ca59e
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-08-10 10:29:38 +0300

    feat: Enhance JWT payload with useful info

--- src/utility/jwt.go
+++ src/utility/jwt.go
@@ -6,23 +6,31 @@ import (
 	"github.com/ssanyoq/go-boilerplate/src/models"
 	"os"
 	"strconv"
+	"time"
 )
+
+type JWTClaims struct {
+	id        int64
+	name      string
+	expiresAt int
+}
 
 // SetTokens sets necessary tokens for further authentication.
 // It requires *gin.Context to set the token in cookies
 //
 // Sooner or later I'll add second token for complete OAuth thing ._.
 func SetTokens(c *gin.Context, user models.User) error {
+	jwtAge, _ := strconv.ParseInt(os.Getenv("JWT_AGE_S"), 10, 64)
 	payload := jwt.MapClaims{
-		"id":   user.ID,
-		"name": user.Name,
+		"id":        user.ID,
+		"name":      user.Name,
+		"expiresAt": time.Now().Second() + int(jwtAge),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 	signedString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
 	if err != nil {
 		return err
 	}
-	jwtAge, _ := strconv.ParseInt(os.Getenv("JWT_AGE_S"), 10, 64)
 	c.SetCookie(
 		"jwt",
 		signedString,
